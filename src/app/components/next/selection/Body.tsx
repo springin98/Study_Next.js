64c7c8cd2e33a0868a2683f77272fc0971d9c536
김하림 <hr.kim1@naturemobility.com>
2023-11-29 13:54:26 +0900

    다중 체크박스 ( 버튼 비활성화 기능 O )

--- src/app/components/next/selection/Body.tsx
+++ src/app/components/next/selection/Body.tsx
@@ -1,3 +1,4 @@
+import MultipleCheck from "./MultipleCheck";
 import MultipleSelection from "./MultipleSelection";
 import SingleSelection from "./SingleSelection";
 
@@ -18,6 +19,14 @@ export default function Body() {
         <li>전체 : O - 옵션 전부 선택하면 자동으로 [전체] 선택</li>
       </ul>
       <MultipleSelection />
+
+      <h1>다중 체크박스</h1>
+      <ul>
+        <li>Default : X</li>
+        <li>전체 : O - 옵션 전부 선택하면 자동으로 [전체] 선택</li>
+        <li>아무것도 선택되지 않을 수 있음</li>
+      </ul>
+      <MultipleCheck />
     </>
   );
 }
